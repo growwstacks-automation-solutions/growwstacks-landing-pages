7739f94f14716ee03a9c9cb63891079307f52463
shubhampatel1010 <shubhampatel@growwstacks.com>
2026-03-30 16:42:18 +0530

    update gallery file via API

--- about/gallery-data.js
+++ about/gallery-data.js
@@ -1,5 +1,22 @@
 var GALLERY_DATA = [
   {
+    id: 2,
+    type: "vid",
+    thumb: "https://img.youtube.com/vi/rAVIJnZlXP0/hqdefault.jpg",
+    alt: "Turf Activity V1",
+    title: "Turf Activity V1",
+    tag: "Sports Reel",
+    video: "https://www.youtube.com/shorts/rAVIJnZlXP0"
+  },
+  {
+    id: 3,
+    type: "img",
+    src: "https://ik.imagekit.io/r2zdlyze2o/Office%20/Activity-Growwstacks/RNR5.png",
+    alt: "Make Platinum Celebration",
+    title: "Make Platinum Celebration",
+    tag: "Spirit Day"
+  },
+  {
     id: 1,
     type: "img",
     src: "https://ik.imagekit.io/r2zdlyze2o/Office%20/Activity-Growwstacks/Christmascelebration.png",
