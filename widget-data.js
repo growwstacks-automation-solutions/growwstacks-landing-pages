d29fc0e06aba3bd361355d9c449759a039f34a56
shubhampatel1010 <shubhampatel@growwstacks.com>
2026-04-03 16:44:10 +0530

    growwstacks widget update by API

--- widget-data.js
+++ widget-data.js
@@ -10,5 +10,145 @@ export const widgetData = [
     "logoUrl": "https://ik.imagekit.io/r2zdlyze2o/GS-Website/Widget/Julian/Org.png",
     "mapimageUrl": "https://ik.imagekit.io/r2zdlyze2o/GS-Website/Widget/Julian/Map.png",
     "ProjectStatus": "Active"
+  },
+  {
+    "clientName": "Austin Jones",
+    "projectName": "Equipment Repair User Capture Setup",
+    "projectType": "Automation",
+    "country": "USA",
+    "city": "New Jersey",
+    "logoUrl": "https://ik.imagekit.io/r2zdlyze2o/GS-Website/Widget/Austin%20Jones/image.png",
+    "mapimageUrl": "https://ik.imagekit.io/r2zdlyze2o/GS-Website/Widget/Map_Image/image_7_3PaoQzm.png",
+    "ProjectStatus": "Complete - Payment Pending"
+  },
+  {
+    "clientName": "Sri Vasavi",
+    "projectName": "Whatsapp Bot for Matrimonial",
+    "projectType": "Automation",
+    "country": "India",
+    "city": "Banglore",
+    "logoUrl": "https://ik.imagekit.io/r2zdlyze2o/Growwstacks%20widget/logo.png",
+    "mapimageUrl": "https://ik.imagekit.io/r2zdlyze2o/GS-Website/Widget/Map_Image/image_A114OtwBF.png",
+    "ProjectStatus": "Client Pending"
+  },
+  {
+    "clientName": "Aryan Shirani Bid Abadi",
+    "projectName": "Ringostat to Airtable",
+    "projectType": "Automation",
+    "country": "Italy",
+    "city": "Calliano TN",
+    "logoUrl": "https://ik.imagekit.io/r2zdlyze2o/GS-Website/Widget/Aryan%20Shirani%20Bid%20Abadi/image.png",
+    "mapimageUrl": "https://ik.imagekit.io/r2zdlyze2o/GS-Website/Widget/Map_Image/image_qckHaWTCE.png",
+    "ProjectStatus": "Complete - Payment Pending"
+  },
+  {
+    "clientName": "Dana Wizman Danan",
+    "projectName": "Automated Invoice Management System",
+    "projectType": "Automation",
+    "country": "United Kingdom",
+    "city": "London",
+    "logoUrl": "https://ik.imagekit.io/r2zdlyze2o/GS-Website/Widget/Dana%20Danan/image%20(1).jpg",
+    "mapimageUrl": "https://ik.imagekit.io/r2zdlyze2o/GS-Website/Widget/Map_Image/image_DltsXOaL2.png",
+    "ProjectStatus": "Complete - Payment Pending"
+  },
+  {
+    "clientName": "Tim Pascual",
+    "projectName": "Lead-to-Demo Booking Automation",
+    "projectType": "Automation",
+    "country": "Germany",
+    "city": "Berlin",
+    "logoUrl": "https://ik.imagekit.io/r2zdlyze2o/GS-Website/Widget/Tim%20Pascual/image.png",
+    "mapimageUrl": "https://ik.imagekit.io/r2zdlyze2o/GS-Website/Widget/Map_Image/image_wXSKbzxSr.png",
+    "ProjectStatus": "Client Pending"
+  },
+  {
+    "clientName": "Richard Bradford",
+    "projectName": "Two way sales order sync and production schedule automation",
+    "projectType": "Automation",
+    "country": "USA",
+    "city": "Salt lake city",
+    "logoUrl": "https://ik.imagekit.io/r2zdlyze2o/Growwstacks%20widget/image.png",
+    "mapimageUrl": "https://ik.imagekit.io/r2zdlyze2o/GS-Website/Widget/Map_Image/image_A-HhxREUO.png",
+    "ProjectStatus": "In Progress"
+  },
+  {
+    "clientName": "Dora silvia",
+    "projectName": "AI-Powered Lead Scraping Automation",
+    "projectType": "Automation",
+    "country": "UT",
+    "city": "Jordan",
+    "logoUrl": "https://ik.imagekit.io/r2zdlyze2o/Growwstacks%20widget/image.png",
+    "mapimageUrl": "https://ik.imagekit.io/r2zdlyze2o/GS-Website/Widget/Map_Image/image_O-t5pvrcj.png",
+    "ProjectStatus": "In Progress"
+  },
+  {
+    "clientName": "Willem Greeff",
+    "projectName": "Unified Lead Routing & Communication Automation",
+    "projectType": "Automation",
+    "country": "South Africa",
+    "city": "Matilda",
+    "logoUrl": "https://ik.imagekit.io/r2zdlyze2o/GS-Website/Widget/Willem%20Greeff/image%20(2).png",
+    "mapimageUrl": "https://ik.imagekit.io/r2zdlyze2o/GS-Website/Widget/Map_Image/image_5gNuL2z4W.png",
+    "ProjectStatus": "In Progress"
+  },
+  {
+    "clientName": "Gregory Van Loock",
+    "projectName": "Contact Form Submission Sync to Mailchimp (English & French)",
+    "projectType": "Automation",
+    "country": "Belgium",
+    "city": "Bruges",
+    "logoUrl": "https://ik.imagekit.io/r2zdlyze2o/GS-Website/Widget/Gregory%20Van/unikpools.png",
+    "mapimageUrl": "https://ik.imagekit.io/r2zdlyze2o/GS-Website/Widget/Map_Image/image_rnkUc4dpl.png",
+    "ProjectStatus": "Completed"
+  },
+  {
+    "clientName": "Monika Miedzianowska",
+    "projectName": "Zadarma Lead Capture Automation System",
+    "projectType": "Automation",
+    "country": "Netherlands",
+    "city": "Drenthe ",
+    "logoUrl": "https://ik.imagekit.io/r2zdlyze2o/GS-Website/Widget/Monika%20Miedzianowska/image.png",
+    "mapimageUrl": "https://ik.imagekit.io/r2zdlyze2o/GS-Website/Widget/Map_Image/image_rm9mPCJL_.png",
+    "ProjectStatus": "Completed"
+  },
+  {
+    "clientName": "Dmitry Loukine",
+    "projectName": "Pipedrive Technical Implementation- Inventory management",
+    "projectType": "Automation",
+    "country": "Australia",
+    "city": "sydney",
+    "logoUrl": "https://ik.imagekit.io/r2zdlyze2o/GS-Website/Widget/Dmitry%20Loukine/image.png",
+    "mapimageUrl": "https://ik.imagekit.io/r2zdlyze2o/GS-Website/Widget/Map_Image/image_4YwsI9hR-.png",
+    "ProjectStatus": "Completed"
+  },
+  {
+    "clientName": "Michael Edwards",
+    "projectName": "Pipedrive-Airtable-Cloud Voice Integration",
+    "projectType": "Automation",
+    "country": "United Kingdom",
+    "city": "Northern Ireland",
+    "logoUrl": "https://ik.imagekit.io/r2zdlyze2o/GS-Website/Widget/Michael%20Edwards/unify.png",
+    "mapimageUrl": "https://ik.imagekit.io/r2zdlyze2o/GS-Website/Widget/Map_Image/image_iGuvnMIID.png",
+    "ProjectStatus": "Completed"
+  },
+  {
+    "clientName": "Dr Aaron Smith",
+    "projectName": "Invoice Scheduling & Auto-Pay Setup Using Make, Cognito Forms, & Stripe",
+    "projectType": "Automation",
+    "country": "US",
+    "city": "California",
+    "logoUrl": "https://ik.imagekit.io/r2zdlyze2o/Growwstacks%20widget/image.png",
+    "mapimageUrl": "https://ik.imagekit.io/r2zdlyze2o/GS-Website/Widget/Map_Image/image_6pXPnK6m0.png",
+    "ProjectStatus": "Complete - Payment Pending"
+  },
+  {
+    "clientName": "Harshbir Singh",
+    "projectName": "Supplier Invoice Price Tracking & Management System",
+    "projectType": "Automation",
+    "country": "UK",
+    "city": "Hayes",
+    "logoUrl": "https://ik.imagekit.io/r2zdlyze2o/Growwstacks%20widget/image.png",
+    "mapimageUrl": "https://ik.imagekit.io/r2zdlyze2o/GS-Website/Widget/Map_Image/image_b_M7cpTZr.png",
+    "ProjectStatus": "In Progress"
   }
 ];
