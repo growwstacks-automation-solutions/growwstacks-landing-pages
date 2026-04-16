2b68105e1bc2082e01cb60c7b14b2f52a93b64c3
shubhampatel1010 <shubhampatel@growwstacks.com>
2026-04-16 19:23:18 +0530

    case-studies apps card update via API

--- _shared/components/service-app-cart.js
+++ _shared/components/service-app-cart.js
@@ -9,5 +9,18 @@ window.templateData = [
     "displayTitle": "Automate Airtable Invoice Uploads to Google Drive",
     "Industry": "Financial Services",
     "ROI": "200"
+  },
+  {
+    "title": "Automate Employment Contract PDF Storage and Airtable Tracking",
+    "description": "This workflow automatically captures employment contract data via webhook, uploads the PDF to Google Drive, generates a shareable link, and logs all details in Airtable for easy HR management. Ideal for agencies handling domestic helper contracts needing organized digital record-keeping and compliance.",
+    "image": "https://ik.imagekit.io/r2zdlyze2o/Case_Studies/Apps/desired_image_name_3Zuwy9fVi.jpg",
+    "apps": [
+      "Google Drive",
+      "Airtable"
+    ],
+    "driveUrl": "https://ik.imagekit.io/r2zdlyze2o/Case_Studies/Apps/desired_image_name_3Zuwy9fVi.jpg",
+    "Industry": "Human Resources",
+    "ROI": "200",
+    "displayTitle": "Automate Employment Contract PDF Storage and Airtable Tracking"
   }
 ];;
